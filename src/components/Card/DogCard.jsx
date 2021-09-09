79d788ca76858e9899a98f1294737db5922f6148
Daniel Perez <daniel@secret-source.eu>
2021-09-09 15:45:20 +0100

    Displaying dog votes in real time and adding styles

--- src/components/Card/DogCard.jsx
+++ src/components/Card/DogCard.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link, Route } from 'wouter';
 
 import './css/dogCard.scss';
 import DogInfo from '../DogInfo';
-import voteSubmitter from '../../helpers/voteSubmitter';
 import dogVotes from '../../helpers/dogVotes';
+import voteSubmitter from '../../helpers/voteSubmitter';
 
 const DogCard = ({ dog, votes }) => {
   const { breeds } = dog || {};
   const { id, url } = dog || '';
   const name = breeds[0] ? breeds[0].name : '';
-  const currentDogVotes = dogVotes(id, votes);
+
+  const [currentDogVotes, setCurrentDogVotes] = useState(dogVotes(id, votes));
+  const [showDogInfo, setShowDogInfo] = useState(false);
 
   return (
-    <Card className="mb-4" style={{ width: '18rem', paddingLeft: 0, paddingRight: 0 }}>
+    <Card className="mt-4" style={{ width: '18rem', paddingLeft: 0, paddingRight: 0 }}>
       <Card.Header className="text-center">
         <a href={url} target="_blank" rel="noreferrer">
           <img src={url} height="200" width="200" alt={`dog id ${id}`} />
@@ -27,23 +28,34 @@ const DogCard = ({ dog, votes }) => {
           <strong>{name}</strong>
         </p>
         <div className="wrapper">
-          <FontAwesomeIcon className="icon-left" icon={faThumbsUp} onClick={() => voteSubmitter(id, 1)} />
-          <span className="vote-count">
+          <FontAwesomeIcon
+            className="icon-left"
+            icon={faThumbsUp}
+            onClick={() => {
+              setCurrentDogVotes(currentDogVotes + 1);
+              voteSubmitter(id, 1);
+            }}
+            size="lg"
+          />
+          <strong className="vote-count">
             {currentDogVotes}
-          </span>
-          <FontAwesomeIcon className="icon-right" icon={faThumbsDown} onClick={() => voteSubmitter(id, 0)} />
+          </strong>
+          <FontAwesomeIcon
+            className="icon-right"
+            icon={faThumbsDown}
+            onClick={() => {
+              setCurrentDogVotes(currentDogVotes - 1);
+              voteSubmitter(id, 0);
+            }}
+            size="lg"
+          />
         </div>
         <br />
-        <Link to={`/dog/${id}`} className="button-link">
-          <Button style={{ justifyContent: 'center', display: 'flex' }}>
-            More info
-          </Button>
-        </Link>
-
-        <Route
-          component={() => <DogInfo dog={dog} />}
-          path={`/dog/${id}`}
-        />
+        <Button onClick={() => setShowDogInfo(!showDogInfo)} style={{ display: 'flex', margin: '0 auto' }}>
+          More info
+        </Button>
+        {showDogInfo
+          && <DogInfo dog={dog} />}
       </Card.Body>
     </Card>
   );

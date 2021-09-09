b2b5862b48ba0a84b09633808ce2e6f89a26ef27
Daniel Perez <daniel@secret-source.eu>
2021-09-09 15:45:49 +0100

    Adding styles to "breeds" page

--- src/screens/Breeds.jsx
+++ src/screens/Breeds.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+
+import './css/breeds.scss';
 
 const Breeds = ({ params }) => {
   const [breedGroupList, setBreedGroupList] = useState({});
 
   const apiKey = process.env.REACT_APP_API_KEY;
   const { breedGroup } = params;
+
   const breedFetch = async () => fetch(`https://api.thedogapi.com/v1/breeds/search?q=${breedGroup}`,
     {
       headers: new Headers({
@@ -21,18 +25,18 @@ const Breeds = ({ params }) => {
   }, []);
 
   return (
-    <div>
+    <Container>
       <h2>
         {breedGroup} breed group
       </h2>
-      <ul>
+      <ul className="ul-columns">
         {breedGroupList.length > 0 && breedGroupList.map((dog) => (
           <li key={dog}>
             {dog.name}
           </li>
         ))}
       </ul>
-    </div>
+    </Container>
   );
 };
 
